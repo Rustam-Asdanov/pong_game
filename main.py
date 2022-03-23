8ce519e5c2604abfe2a02b58ac064ae0d9255de8
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-03-23 14:30:50 +0400

    Menu for exit is created

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import time
 from turtle import Screen
 from game_board import GameBoard, BOARD_HEIGHT, BOARD_WIDTH
 from player import Player
 from score_board import ScoreBoard
+from time import sleep
 
 # Screen properties
 screen = Screen()
 screen.setup(width=BOARD_WIDTH, height=BOARD_HEIGHT)
 screen.title("Pong game")
 screen.bgcolor("black")
+screen.listen()
+game_over = False
+
+
+def exit_game():
+    user_choice = screen.textinput(title="Exit",
+                                   prompt="Do you want to exit? (y/n)")
+
+    if user_choice == "y":
+        global game_over
+        game_over = True
+        screen.bye()
+
 
 # Game board properties
 board = GameBoard()
@@ -19,7 +34,17 @@ score = ScoreBoard()
 # Players properties
 player_1 = Player("blue")
 player_1.start_position("left")
+screen.onkeypress(fun=player_1.move_up, key="w")
+screen.onkeypress(fun=player_1.move_down, key="s")
 player_2 = Player("green")
 player_2.start_position("right")
+screen.onkeypress(fun=player_2.move_up, key="Up")
+screen.onkeypress(fun=player_2.move_down, key="Down")
+
+screen.onkey(fun=exit_game, key="Escape")
+
+while not game_over:
+    screen.update()
+
 
 screen.exitonclick()
--- player.py
+++ player.py
@@ -7,7 +7,8 @@ class Player(Turtle):
     def __init__(self, color):
         super().__init__()
         self.color(color)
-        self.shapesize(stretch_wid=3, stretch_len=0.7)
+        self.shapesize(stretch_wid=0.7, stretch_len=3)
+        self.setheading(90)
         self.shape("square")
         self.penup()
 
@@ -16,3 +17,11 @@ class Player(Turtle):
             self.setposition(-1 * BOARD_WIDTH / 2 + 20, 0)
         if position == "right":
             self.setposition(BOARD_WIDTH / 2 - 20, 0)
+
+    def move_up(self):
+        if self.ycor() < BOARD_HEIGHT / 2 - 40:
+            self.forward(15)
+
+    def move_down(self):
+        if self.ycor() > -1 * BOARD_HEIGHT / 2 + 40:
+            self.backward(15)
